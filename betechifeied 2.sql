3800297105e6f8170c3493a8f44f9b0f144584de
Esohe-Divine <eghomwanre17@gmail.com>
2026-03-25 03:55:29 -1200

    Update README WITH PROJECT DETAILS AND QUERIES
    Refactored SQL queries for employee data retrieval and salary statistics.

--- betechifeied 2.sql
+++ betechifeied 2.sql
@@ -1,33 +1,40 @@
-select s.first_name, s.last_name, s.occupation, p.department_name
-from parks_and_recreation.employee_salary as s
-join parks_and_recreation.parks_departments as p
-on s.dept_id = p.department_id;
+SELECT 
+  t1.first_name, 
+  t1.last_name,
+  t2.occupation,
+  t3.department_name
+FROM parks_and_recreation.employee_demographics AS t1
+JOIN parks_and_recreation.employee_salary AS t2
+ON t1.employee_id = t2.employee_id
+JOIN parks_and_recreation.parks_departments AS t3
+ON t2.dept_id = t3.department_id;
 
 
-select department_name, avg (salary), min(salary), max(salary)
+select department_name, 
+ avg (salary) AS AVERAGE_SALARY,
+ min(salary) AS MINIMUM_SALARY, 
+ max(salary) AS MAXIMUM_SALARY
 from parks_and_recreation.employee_salary 
 join parks_and_recreation.parks_departments 
 on dept_id = department_id
  group by department_name;
 
 
-select avg(salary)
- from parks_and_recreation.employee_salary
 
 
-
-
-select first_name, last_name, salary 
- from parks_and_recreation.employee_salary
- where (select avg(salary)
- from parks_and_recreation.employee_salary)
+SELECT 
+  first_name, 
+  last_name, 
+  salary 
+ FROM parks_and_recreation.employee_salary
+ WHERE  salary > (SELECT AVG(salary) FROM parks_and_recreation.employee_salary);
  
 
 
-select d.first_name, d.last_name, ifnull(s.occupation, 'not assigned') as occupation 
-from parks_and_recreation.employee_demographics as d
-left join parks_and_recreation.employee_salary as s
-on d.employee_id = s.employee_id;
+SELECT d.first_name, d.last_name, IFNULL(s.occupation, 'not assigned') as occupation 
+FROM parks_and_recreation.employee_demographics AS d
+LEFT JOIN parks_and_recreation.employee_salary AS s
+ON d.employee_id = s.employee_id;
  
 
  
